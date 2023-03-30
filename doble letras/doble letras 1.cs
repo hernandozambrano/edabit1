7664877fdb8fe472552d4cb97e46d27f8cfc8451
tatiana zambrano <tatiana zambrano@TATIANA-ZAMBRAN>
2023-03-30 13:08:32 -0500

    Agregar archivos de proyecto.

---
+++ doble letras/doble letras 1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doble_letras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(dobleletras("pErto"));
+            Console.WriteLine(dobleletras("soyelmejorr"));
+            Console.ReadKey();
+        }
+        public static Boolean dobleletras(string palabra)
+        {
+            char[] chars = palabra.ToUpper().ToCharArray();
+            for (int c =0;c<chars.Length-1;c++)
+            {
+                if (chars[c] == chars[c + 1])
+                return true;
+            }
+            return false;
+        } 
+    }
+
+}
+  
